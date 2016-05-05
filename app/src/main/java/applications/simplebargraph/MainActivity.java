fbd6abdc2f9069220a6dfba8e55ac8974c88fb29
mitchtabian <mitch@tabian.ca>
2016-05-04 19:48:41 -0700

    Source Code

---
+++ app/src/main/java/applications/simplebargraph/MainActivity.java
@@ -0,0 +1,96 @@
+package applications.simplebargraph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    BarChart barChart;
+    ArrayList<String> dates;
+    Random random;
+    ArrayList<BarEntry> barEntries;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
+        createRandomBarGraph("2016/05/05", "2016/06/01");
+
+    }
+
+    public void createRandomBarGraph(String Date1, String Date2){
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+        try {
+            Date date1 = simpleDateFormat.parse(Date1);
+            Date date2 = simpleDateFormat.parse(Date2);
+
+            Calendar mDate1 = Calendar.getInstance();
+            Calendar mDate2 = Calendar.getInstance();
+            mDate1.clear();
+            mDate2.clear();
+
+            mDate1.setTime(date1);
+            mDate2.setTime(date2);
+
+            dates = new ArrayList<>();
+            dates = getList(mDate1,mDate2);
+
+            barEntries = new ArrayList<>();
+            float max = 0f;
+            float value = 0f;
+            random = new Random();
+            for(int j = 0; j< dates.size();j++){
+                max = 100f;
+                value = random.nextFloat()*max;
+                barEntries.add(new BarEntry(value,j));
+            }
+
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+        BarData barData = new BarData(dates,barDataSet);
+        barChart.setData(barData);
+        barChart.setDescription("My First Bar Graph!");
+
+    }
+
+    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
+        ArrayList<String> list = new ArrayList<String>();
+        while(startDate.compareTo(endDate)<=0){
+            list.add(getDate(startDate));
+            startDate.add(Calendar.DAY_OF_MONTH,1);
+        }
+        return list;
+    }
+
+    public String getDate(Calendar cld){
+        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
+                +cld.get(Calendar.DAY_OF_MONTH);
+        try{
+            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
+            curDate =  new SimpleDateFormat("yyy/MM/dd").format(date);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return curDate;
+    }
+
+}
